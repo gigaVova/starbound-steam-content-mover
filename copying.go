09c2891bdc45126c1af37ee1a7d9286e33d816c2
gigaVova <gigavova@keemail.me>
2024-03-12 11:22:09 +0300

    add a struct for directories

--- copying.go
+++ copying.go
@@ -38,10 +38,10 @@ func copyFile(src, dst string) error {
 	return nil
 }
 
-func copyFiles(srcDirectoryPath, targetDirectoryPath string, wg *sync.WaitGroup) {
+func copyFiles(d directories, wg *sync.WaitGroup) {
 	wg.Add(1)
 	// all entries (directories) of source folder
-	entries, err := os.ReadDir(srcDirectoryPath)
+	entries, err := os.ReadDir(*d.targetDirectoryPath)
 	if err != nil {
 		panic(err)
 	}
@@ -54,7 +54,7 @@ func copyFiles(srcDirectoryPath, targetDirectoryPath string, wg *sync.WaitGroup)
 			// save the name (steam id)
 			currentDirectoryName := entry.Name()
 			// path to the current directory
-			folderPath := filepath.Join(srcDirectoryPath, currentDirectoryName)
+			folderPath := filepath.Join(*d.srcDirectoryPath, currentDirectoryName)
 
 			// all entries (files) of directory with content
 			files, err := os.ReadDir(folderPath)
@@ -65,12 +65,13 @@ func copyFiles(srcDirectoryPath, targetDirectoryPath string, wg *sync.WaitGroup)
 			// going through all the entries
 			for _, file := range files {
 				if !file.IsDir() {
+					// check if the current file is ".pak"
 					if strings.Split(file.Name(), ".")[1] == "pak" {
 						currentFileName := file.Name()
 						currentFilePath := filepath.Join(folderPath, currentFileName)
 
 						newFileName := currentDirectoryName + ".pak"
-						newFilePath := filepath.Join(targetDirectoryPath, newFileName)
+						newFilePath := filepath.Join(*d.targetDirectoryPath, newFileName)
 
 						err := copyFile(currentFilePath, newFilePath)
 						if err != nil {
--- main.go
+++ main.go
@@ -6,21 +6,27 @@ import (
 	"sync"
 )
 
+type directories struct {
+	srcDirectoryPath    *string
+	targetDirectoryPath *string
+}
+
 func main() {
 	var wg sync.WaitGroup
+	var d directories
 	// spinner lol
 	s := chin.New().WithWait(&wg)
 
 	// directory we want to get content from
-	var srcDirectoryPath = flag.String("src", "", "directory to move content from")
+	d.srcDirectoryPath = flag.String("src", "", "directory to move content from")
 	// directory we want to move content to
-	var targetDirectoryPath = flag.String("target", "./content", "directory to move content to")
+	d.targetDirectoryPath = flag.String("target", "./content", "directory to move content to")
 	flag.Parse()
 
-	checkTargetDirectory(*targetDirectoryPath)
+	checkTargetDirectory(*d.targetDirectoryPath)
 
 	go s.Start()
-	copyFiles(*srcDirectoryPath, *targetDirectoryPath, &wg)
+	copyFiles(d, &wg)
 	s.Stop()
 	wg.Wait()
 }
